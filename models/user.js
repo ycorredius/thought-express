a979b2fbeec2d90a90e9078c5f55c2533a260244
shaun <ycorredius@gmail.com>
2021-09-28 20:48:37 -0400

    added npm packages and user model

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose =  require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    userHandle:{
+        type: String,
+        required: true
+    },
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required:true
+    }
+})
+
+module.exports = User = mongoose.model('user', userSchema)
